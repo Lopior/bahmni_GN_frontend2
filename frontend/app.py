d8af354ee04be85505b77e22a5ff3abf86bcd6d3
juanjortega <juanjortega@gmail.com>
2024-04-09 17:38:52 -0400

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,38 @@
-from flask import Flask, render_template, jsonify, request, redirect, make_response
+from flask import Flask, render_template, jsonify, request
 import requests
 import json
 import datetime
 from dotenv import load_dotenv
 import os
 import pdfkit
+from flask import make_response
 from weasyprint import HTML
 
 app = Flask(__name__)
+
+#se agrega esta especie de middleware para que funcione con el reverse proxy
+
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+            print("SCRIPT_NAME original:", environ.get('SCRIPT_NAME'))
+            print("PATH_INFO original:", environ.get('PATH_INFO'))
+
+            script_name = environ.get('HTTP_X_FORWARDED_PREFIX', '')
+            if script_name:
+                environ['SCRIPT_NAME'] = script_name
+                path_info = environ['PATH_INFO']
+                if path_info.startswith(script_name):
+                    environ['PATH_INFO'] = path_info[len(script_name):]
+
+            print("SCRIPT_NAME modificado:", environ.get('SCRIPT_NAME'))
+            print("PATH_INFO modificado:", environ.get('PATH_INFO'))
+
+            return self.app(environ, start_response)
+
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 load_dotenv()
 url_backend = os.getenv('url_backend_var')
@@ -16,11 +41,12 @@ url_backend = os.getenv('url_backend_var')
 path_wkhtmltopdf = '/usr/bin/wkhtmltopdf'
 config_pdf = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
-@app.route('/')
+
+@app.route('/notificacion')
 def index():
     return render_template('index.html')
 
-@app.route('/api/notificacionges/<string:id_ges>', methods=['GET'])
+@app.route('/notificacionges/<string:id_ges>', methods=['GET'])
 def get_ges_data(id_ges):
     try:
         # Hacer la solicitud GET a la API
@@ -32,26 +58,40 @@ def get_ges_data(id_ges):
         if response.status_code == 200:
             data = response.json()
             # Procesar los datos obtenidos
-            # consultar el person id y name del practitioner
+            #consultar el person id y name del practitioner
             response_practitioner = requests.get(f'{url_backend}/practitioner/{practitioner_user}')
             
             if response_practitioner.status_code == 200:
+                
                 data_practitioner = response_practitioner.json()
+                print("*********** DATOS PRACTITIONER ***********")
+                print (response_practitioner)
+                print(practitioner_user)
+                print(data_practitioner)
+                print(json.dumps(data_practitioner, indent=4))
                 data['notificador_id'] = data_practitioner['person_id']
                 data['nombre_notificador'] = data_practitioner['given_name']+' '+data_practitioner['family_name']
+                #print(response.text)  # Imprimir el contenido de la respuesta en la consola
+                print("*********** DATOS DESDE API BACKEND ***********")
+            
                 # Extraer fecha y hora actual
                 now = datetime.datetime.now()
                 data['fechahora_notificacion'] = now.strftime("%Y-%m-%d %H:%M:%S")
+                print(data)
                 return render_template('form_ges.html', data=data)
+
             else:
+                # Manejar el caso de error en la solicitud
                 return 'Error al obtener los datos del practitioner', 500
+
         else:
+            # Manejar el caso de error en la solicitud
             return 'Error al obtener los datos del caso', 500
     except Exception as e:
         print("Error al notificar GES:", str(e))
         return jsonify({'cod': 'error', 'message': 'error al notificar GES'+str(e)})
     
-@app.route('/api/notificacionges', methods=['POST'])
+@app.route('/notificacionges', methods=['POST'])
 def post_ges_data():
     data = dict(request.form)
     # Hacer la solicitud POST al backend
@@ -60,29 +100,40 @@ def post_ges_data():
     if response.status_code == 200:
         return render_template('form_ges_response.html', data=response.json())
     else:
+        # Manejar el caso de error en la solicitud
         return 'Error al enviar los datos al backend', 500
 
-@app.route('/api/notificaciongespaciente/<string:uuid_notificacion>', methods=['GET'])
+@app.route('/notificaciongespaciente/<string:uuid_notificacion>', methods=['GET'])
 def get_ges_paciente_data(uuid_notificacion):
     # Hacer la solicitud GET a la API
     response = requests.get(f'{url_backend}/ges?uuid={uuid_notificacion}')
     # Verificar si la solicitud fue exitosa (código de estado 200)
     if response.status_code == 200:
         data = response.json()
+        # Procesar los datos obtenidos
+        #print(response.text)  # Imprimir el contenido de la respuesta en la consola
+        print("*********** DATOS DESDE API BACKEND ***********")
+        print(data)
         return render_template('form_ges_paciente.html', data=data)
     else:
+        # Manejar el caso de error en la solicitud
         return 'Error al obtener los datos del caso', 500
+    #data = "{}"
+    #return render_template('form_ges_paciente.html', data=data)
 
-@app.route('/api/notificaciongespaciente', methods=['POST'])
+@app.route('/notificaciongespaciente', methods=['POST'])
 def post_ges_data_paciente():
     try:
         data = dict(request.form)
+        print(data)
+        print(data["firma_paciente"])
         # Hacer la solicitud POST al backend
         response = requests.post(url_backend+'/ges/firma', json=json.dumps(data))
         # Verificar si la solicitud fue exitosa (código de estado 200)
         if response.status_code == 200:
             return render_template('form_ges_response.html', data=response.json())
         else:
+            # Manejar el caso de error en la solicitud
             return 'Error al enviar los datos al backend', 500
     except Exception as e:
         print("Error al firmar notificacion GES:", str(e))
@@ -95,26 +146,49 @@ def view_ges_data(id_ges):
     # Verificar si la solicitud fue exitosa (código de estado 200)
     if response.status_code == 200:
         data = response.json()
+        # Procesar los datos obtenidos
+        #print(response.text)  # Imprimir el contenido de la respuesta en la consola
+        print("*********** DATOS DESDE API BACKEND ***********")
+        print(data)
         return render_template('form_ver_ges.html', data=data)
     else:
+        # Manejar el caso de error en la solicitud
         return 'Error al obtener los datos del caso', 500
 
-@app.route('/api/vernotificacionges/pdf/<string:id_ges>', methods=['GET'])
+
+#pasar html a pdf
+@app.route('/vernotificacionges/pdf/<string:id_ges>', methods=['GET'])
 def view_ges_data_pdf(id_ges):
+    #instalar en so
+    #brew install wkhtmltopdf
+    #html = "<h1>Hola mundo</h1>"
     html = view_ges_data(id_ges)
+    # Convierte el HTML a PDF
     pdf = pdfkit.from_string(html, False, configuration=config_pdf)
+
+    # Crea una respuesta con el PDF
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+
     return response
 
-@app.route('/api/vernotificacionges/pdf2/<string:id_ges>', methods=['GET'])
+@app.route('/vernotificacionges/pdf2/<string:id_ges>', methods=['GET'])
 def view_ges_data_pdf2(id_ges):
+    #instalar en so
+    #brew install pango gdk-pixbuf cairo
+    # Obtén tu HTML
+    #html = "<h1>Hola mundo</h1>"
     html = view_ges_data(id_ges)
+
+    # Convierte el HTML a PDF
     pdf = HTML(string=html).write_pdf()
+
+    # Crea una respuesta con el PDF
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
+
     return response
 
 if __name__ == '__main__':
