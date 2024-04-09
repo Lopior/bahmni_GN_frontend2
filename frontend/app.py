bc71874502195e6806e09c9ec099063d6f85a1e1
juanjortega <juanjortega@gmail.com>
2024-04-09 14:44:17 -0400

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -11,17 +11,25 @@ from weasyprint import HTML
 app = Flask(__name__)
 
 #se agrega esta especie de middleware para que funcione con el reverse proxy
+
 class ReverseProxied(object):
     def __init__(self, app):
         self.app = app
 
     def __call__(self, environ, start_response):
+        print("SCRIPT_NAME original:", environ.get('SCRIPT_NAME'))
+        print("PATH_INFO original:", environ.get('PATH_INFO'))
+
         script_name = environ.get('HTTP_X_FORWARDED_PREFIX', '')
         if script_name:
             environ['SCRIPT_NAME'] = script_name
             path_info = environ['PATH_INFO']
             if path_info.startswith(script_name):
                 environ['PATH_INFO'] = path_info[len(script_name):]
+
+        print("SCRIPT_NAME modificado:", environ.get('SCRIPT_NAME'))
+        print("PATH_INFO modificado:", environ.get('PATH_INFO'))
+
         return self.app(environ, start_response)
 
 app.wsgi_app = ReverseProxied(app.wsgi_app)
